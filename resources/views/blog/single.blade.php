3dad0dfce5478cec448fef030a7f1b95ce8cc583
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-02-05 23:14:30 +0100

    Update single.blade.php

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -435,56 +435,62 @@
 
 <!-- MODAL WINDOW (NEWSLETTER FORM)
 			============================================= -->
-<div id="modal-2" class="modal fade" tabindex="-1">
+<!-- MODAL WINDOW (NEWSLETTER FORM)
+			============================================= -->
+<!-- <div id="modal-2" class="modal fade" tabindex="-1">
     <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
+        <div class="modal-content"> -->
 
 
             <!-- CLOSE BUTTON -->
-            <button type="button" class="btn-close ico-10 white-color" data-bs-dismiss="modal" aria-label="Close">
+            <!-- <button type="button" class="btn-close ico-10 white-color" data-bs-dismiss="modal" aria-label="Close">
                 <span class="flaticon-cancel"></span>
-            </button>
+            </button> -->
 
 
             <!-- MODAL CONTENT -->
-            <div class="modal-body text-center">
+            <!-- <div class="modal-body text-center"> -->
 
 
                 <!-- IMAGE -->
-                <div class="modal-body-img">
+                <!-- <div class="modal-body-img">
                     <img class="img-fluid" src="{{asset('images/modal-newsletter-blue.jpg')}}" alt="content-image">
-                </div>
+                </div> -->
 
 
                 <!-- NEWSLETTER FORM -->
-                <div class="modal-body-content">
+                <!-- <div class="modal-body-content"> -->
 
                     <!-- Title -->
-                    <h5 class="s-24 w-700">Stay up to date with our news, ideas and updates</h5>
+                    <!-- <h5 class="s-24 w-700">Stay up to date with our news, ideas and updates</h5> -->
 
                     <!-- Form -->
-                    <form class="newsletter-form purple-focus">
+                    <!-- <form class="newsletter-form">
 
                         <div class="input-group">
                             <input type="email" autocomplete="off" class="form-control" placeholder="Your email address" required id="s-email">
                             <span class="input-group-btn">
                                 <button type="submit" class="btn btn--theme hover--theme">Subscribe Now</button>
                             </span>
-                        </div>
+                        </div> -->
 
                         <!-- Newsletter Form Notification -->
-                        <label for="s-email" class="form-notification"></label>
+                        <!-- <label for="s-email" class="form-notification"></label> -->
 
-                    </form>
+                    <!-- </form> -->
 
-                </div> <!-- END NEWSLETTER FORM -->
-
-
-            </div> <!-- END MODAL CONTENT -->
+                <!-- </div> -->
+                 <!-- END NEWSLETTER FORM -->
 
 
-        </div>
-    </div>
-</div> <!-- END MODAL WINDOW (NEWSLETTER FORM) -->
+            <!-- </div>  -->
+            <!-- END MODAL CONTENT -->
+
+
+        <!-- </div> -->
+    <!-- </div> -->
+<!-- </div> -->
+ <!-- END MODAL WINDOW (NEWSLETTER FORM) -->
+ <!-- END MODAL WINDOW (NEWSLETTER FORM) -->
 
 @endsection
